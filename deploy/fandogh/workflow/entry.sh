437704b4fcb9b8ffffc3969ca54096621ac7c685
Matin <itsmatinhimself@gmail.com>
2021-10-06 14:47:25 +0330

    :bug:

--- deploy/fandogh/workflow/entry.sh
+++ deploy/fandogh/workflow/entry.sh
@@ -2,14 +2,12 @@ username=$1
 password=$2
 
 export IMAGE_URL='reminderimage'
-echo $RELEASE_VERSION
-export TAG=$RELEASE_VERSION
+export TAG=$(git rev-parse --short HEAD)
 
-RELEASE_VERSION=$(rev-parse --short HEAD)
 
 COLLECT_ERROR=True fandogh login --username $username --password $password
 
 COLLECT_ERROR=True fandogh image init --name  $IMAGE_URL
 COLLECT_ERROR=True fandogh image publish --version $RELEASE_VERSION
 
-COLLECT_ERROR=True fandogh service apply -f ./deploy/fandogh/service.yml -p IMAGE_URL -p TAG -p RELEASE_VERSION
+COLLECT_ERROR=True fandogh service apply -f ./deploy/fandogh/service.yml -p IMAGE_URL -p TAG
