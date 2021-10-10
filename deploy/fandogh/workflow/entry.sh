ac60d4c7210ce7ba3e06a9463f36c02bc34aa319
Matin <itsmatinhimself@gmail.com>
2021-10-10 13:44:29 +0330

    fix deploy script

--- deploy/fandogh/workflow/entry.sh
+++ deploy/fandogh/workflow/entry.sh
@@ -1,14 +1,17 @@
 username=$1
 password=$2
-export RELEASE_VERSION=$3
+export TAG=$3
 export IMAGE_URL='reminderimage'
-export TAG=$(git rev-parse --short HEAD)
-
+export COMMIT_SHA=$(git rev-parse HEAD)
 
 
 COLLECT_ERROR=True fandogh login --username $username --password $password
 
-COLLECT_ERROR=True fandogh image init --name  $IMAGE_URL
-COLLECT_ERROR=True fandogh image publish --version $RELEASE_VERSION
+echo "image name: ${IMAGE_URL}"
+echo "image version: ${TAG}"
+echo "commit sha: ${TAG}"
 
-COLLECT_ERROR=True fandogh service apply -f ./deploy/fandogh/service.yml -p IMAGE_URL -p TAG -p RELEASE_VERSION
+COLLECT_ERROR=True fandogh image init --name  $IMAGE_URL
+COLLECT_ERROR=True fandogh image publish --version $TAG
+
+COLLECT_ERROR=True fandogh service apply -f ./deploy/fandogh/service.yml -p IMAGE_URL -p COMMIT_SHA -p TAG
