6fedd27a3ca5dd94fc59e93b0c20b4ca6d58c2bb
Matin <itsmatinhimself@gmail.com>
2022-02-21 22:39:12 +0330

    fix ambiguous git log argument

--- deploy/fandogh/workflow/entry.sh
+++ deploy/fandogh/workflow/entry.sh
@@ -3,13 +3,13 @@ password=$2
 export TAG=$3
 export IMAGE_URL='reminderimage'
 export COMMIT_SHA=$(git rev-parse HEAD)
-export CM=$(git log $(git describe --tags --abbrev=0)^^..HEAD --oneline)
+export CM="$(git log HEAD --oneline)"
 COLLECT_ERROR=True fandogh login --username $username --password $password
 
 echo "image name: ${IMAGE_URL}"
 echo "image version: ${TAG}"
 echo "commit sha: ${COMMIT_SHA}"
-echo "commit messages: ${COMMIT_SHA}"
+echo "commit messages: ${CM}"
 
 COLLECT_ERROR=True fandogh image init --name  $IMAGE_URL
 COLLECT_ERROR=True fandogh image publish --version $TAG
